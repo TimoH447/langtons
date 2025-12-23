e9f9d43e40c9d117d933e328483ccf3e14933fba
Timo <timo.hauser66@gmail.com>
2025-12-23 15:27:29 +0100

    init to change to django less website

---
+++ canvas.js
@@ -0,0 +1,6 @@
+var size = 400
+var c = document.getElementById("myCanvas");
+var context = c.getContext("2d");
+context.clearRect(0,0,size,size);
+context.fillStyle = "green";
+context.fillRect(0,0,size,size);
